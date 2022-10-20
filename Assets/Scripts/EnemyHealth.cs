8b9db86e8936336aa8581801affa28bc3c84fe2d
Newc42 <106112361+Newc42@users.noreply.github.com>
2022-10-20 14:50:44 +0200

    EnemiesUpdate

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class EnemyHealth : MonoBehaviour
     public AudioClip explodeSFX;
     public AudioSource audioSource;
     public GameObject heart;
+
+    public Slider HPSlider;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Bullet")){
